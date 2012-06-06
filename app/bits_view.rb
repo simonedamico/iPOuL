0b539436ffa985ec8793f5f9cfebb581a5c4e722
Simone D'Amico <sim@me.com>
2012-06-06 21:50:40 +0200

    added tux logo

--- app/bits_view.rb
+++ app/bits_view.rb
@@ -16,12 +16,22 @@ class BitsView < UIView
       @poul_label.font = UIFont.fontWithName('HelveticaNeue-BoldItalic', size:font_size)
       @poul_label.sizeToFit
       self.addSubview(@poul_label)
+
+
+      @tux_image = UIImageView.alloc.initWithImage(UIImage.imageNamed('tux.png'))
+      @tux_image.sizeToFit
+      @tux_image.bounds = CGRectInset(@tux_image.bounds, 128, 128) if Device.iphone?
+      self.addSubview(@tux_image)
+      
     end
     self
   end
 
   def layoutSubviews
-      @poul_label.center = CGPoint.new(self.bounds.size.width/2,self.bounds.size.height/8)
+    @poul_label.center = CGPoint.new(self.bounds.size.width/2,self.bounds.size.height/8)
+    @tux_image.center = CGPoint.new(self.bounds.size.width/2, self.bounds.size.height/2)
+
+    
   end
   
   
