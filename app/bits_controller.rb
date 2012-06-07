1c864f3c1d6f3c735a3d4eec4dfdb5e4e8989689
Simone D'Amico <sim@me.com>
2012-06-07 19:28:29 +0200

    added a status label

--- app/bits_controller.rb
+++ app/bits_controller.rb
@@ -10,5 +10,21 @@ class BitsController < UIViewController
   def loadView
     self.view = BitsView.alloc.init
   end
+
+  def viewDidLoad
+    update_status
+  end
+
+  def update_status
+    if is_open?
+      self.view.update_status_label(:open)
+    else
+      self.view.update_status_label(:closed)
+    end
+  end
+  
+  def is_open?
+    return false
+  end
   
 end
--- app/bits_view.rb
+++ app/bits_view.rb
@@ -1,4 +1,6 @@
 class BitsView < UIView
+
+  attr_accessor :status_label
   
   def initWithFrame(rect)
     if super(rect)
@@ -22,17 +24,36 @@ class BitsView < UIView
       @tux_image.sizeToFit
       @tux_image.bounds = CGRectInset(@tux_image.bounds, 128, 128) if Device.iphone?
       self.addSubview(@tux_image)
+
+      @status_label = UILabel.alloc.init
+      @status_label.text = "La sede e' aperta"
+      @status_label.textColor = UIColor.whiteColor
+      @status_label.backgroundColor = UIColor.clearColor
+      @status_label.sizeToFit
+      self.addSubview(@status_label)
       
     end
     self
-  end
+  end 
 
   def layoutSubviews
     @poul_label.center = CGPoint.new(self.bounds.size.width/2,self.bounds.size.height/8)
     @tux_image.center = CGPoint.new(self.bounds.size.width/2, self.bounds.size.height/2)
-
+    @status_label.center = CGPoint.new(self.bounds.size.width/2, self.bounds.size.height/8*7)
     
   end
-  
+
+  def update_status_label(status)
+    if status == :open
+      @status_label.text = "La sede e' aperta"
+    elsif status == :closed
+      @status_label.text = "La sede e' chiusa"
+    else
+      @status_label.text = "La sede e' in uno stato indefinito"
+    end
+    
+    @status_label.sizeToFit
+    self.setNeedsLayout
+  end
   
 end
